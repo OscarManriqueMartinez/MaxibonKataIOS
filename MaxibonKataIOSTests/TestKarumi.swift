954936e94d57ba5574f55ab9d86a62f2046d914d
Óscar Manrique <oscar.manrique@vass.es>
2017-11-23 08:32:01 +0100

    test de solicitud de más maxibons por el chat

--- MaxibonKataIOSTests/TestKarumi.swift
+++ MaxibonKataIOSTests/TestKarumi.swift
@@ -14,9 +14,10 @@ class TestKarumi: XCTestCase {
     
 
     func testIfDeveloperGrabMaxibonsThenShouldRestMoreThan2MaxibonsInTheFridge() {
-        let karumi = KarumiHQs()
         
         property("checkIfDeveloperGrabMaxibonsThenShouldRestMoreThan2MaxibonsInTheFridge") <- forAll({ (developer: Developer) -> Testable in
+            
+            let karumi = KarumiHQs()
             
             print("Num Maxibon before: \(karumi.maxibonsLeft)")
             karumi.openFridge(developer)
@@ -26,4 +27,87 @@ class TestKarumi: XCTestCase {
         })
     }
     
+    func testIfDeveloperHungryGrabMaxibonsThenShouldRestMoreThan2MaxibonsInTheFridge() {
+        
+        property("checkIfDeveloperHungryGrabMaxibonsThenShouldRestMoreThan2MaxibonsInTheFridge") <- forAll( Developer.arbitraryHungry){ (developer: Developer) -> Testable in
+            
+            let karumi = KarumiHQs()
+            
+            print("developer.numberOfMaxibonsToGet: \(developer.numberOfMaxibonsToGet)")
+            karumi.openFridge(developer)
+            
+            let maxibons = self.numberMaxibonsFridge(maxibonsLeft: karumi.maxibonsLeft, numberOfMaxibonsToGet: developer.numberOfMaxibonsToGet)
+            
+            print("maxibonsLeft: \(maxibons)")
+            
+            return karumi.maxibonsLeft == maxibons
+        }
+    }
+    
+    func testIfDeveloperNoHungryGrabMaxibonsThenShouldRestMoreThan2MaxibonsInTheFridge() {
+        
+        property("checkIfDeveloperNoHungryGrabMaxibonsThenShouldRestMoreThan2MaxibonsInTheFridge") <- forAll( Developer.arbitraryNotSoHungry){ (developer: Developer) -> Testable in
+            
+            let karumi = KarumiHQs()
+            
+            print("developer.numberOfMaxibonsToGet: \(developer.numberOfMaxibonsToGet)")
+            karumi.openFridge(developer)
+            
+            let maxibons = self.numberMaxibonsFridge(maxibonsLeft: karumi.maxibonsLeft, numberOfMaxibonsToGet: developer.numberOfMaxibonsToGet)
+            
+            print("maxibonsLeft: \(maxibons)")
+            
+            return karumi.maxibonsLeft == maxibons
+        }
+    }
+    
+    func testIfDevelopersGrabMaxibonsThenShouldRestMoreThan2MaxibonsInTheFridge() {
+        
+        property("checkIfDevelopersGrabMaxibonsThenShouldRestMoreThan2MaxibonsInTheFridge") <- forAll(){ (developers: ArrayOf<Developer>) -> Testable in
+            
+            let karumi = KarumiHQs()
+            
+            karumi.openFridge(developers.getArray)
+            
+            return karumi.maxibonsLeft > 2
+        }.verbose
+    }
+
+    func testCheckPrintInSlackBuyMoreMaxibom() {
+        
+        property("checkPrintInSlackBuyMoreMaxibom") <- forAll(Developer.arbitraryHungry){ (developer: Developer) -> Testable in
+            let chat = ChatMock()
+            let karumi = KarumiHQs(chat: chat)
+            
+            karumi.openFridge(developer)
+            
+            return chat.isSend
+        }
+    }
+    
+    
+    
+    func numberMaxibonsFridge(maxibonsLeft: Int, numberOfMaxibonsToGet: Int) -> Int{
+        var maxibons = maxibonsLeft - numberOfMaxibonsToGet
+        
+        if maxibons < 0{
+            maxibons = 10
+        }
+        
+        if maxibons <= 2{
+            maxibons += 10
+        }
+        
+        return maxibons
+    }
+}
+
+class ChatMock: Chat {
+
+    var isSend: Bool = false
+    
+    func send(message: String) {
+        isSend = true
+        print("------->"+message)
+    }
 }
